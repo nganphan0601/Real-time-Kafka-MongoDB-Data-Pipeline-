ea7d4a3977e464a0bf6e497b6ea70d1b9928db4d
Ngan Phan (300180184) <kimngan.9at2@gmail.com>
2025-08-13 18:55:35 -0700

    updated .gitignore

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ import os
 # Load env variables
 load_dotenv()
 
-src_kafka_
+src_kafka_brokers = os.getenv("SRC_BOOTSTRAP_SERVERS")
+src_security_protocol = os.getenv("SRC_SECURITY_PROTOCOL")
+# print(src_kafka_brokers)
